7842ac4e0fc631736d6bc5fe6b5ab4cb44e85aef
courage173 <courageosemwengie@gmail.com>
2020-05-04 18:00:20 +0100

    Updates

---
+++ precache-manifest.b759db86fe9cdb27187a8586955e407a.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b15850c141f36de39eeb7957fe6f27d3",
+    "url": "/abutto-web/index.html"
+  },
+  {
+    "revision": "c0ffa668e570a6715486",
+    "url": "/abutto-web/static/css/main.1cc4a2bf.chunk.css"
+  },
+  {
+    "revision": "0d7d8c7fb59ec1357d00",
+    "url": "/abutto-web/static/js/2.b0164c73.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/abutto-web/static/js/2.b0164c73.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0ffa668e570a6715486",
+    "url": "/abutto-web/static/js/main.6d7bbe74.chunk.js"
+  },
+  {
+    "revision": "94e51b0e8d760dde2109",
+    "url": "/abutto-web/static/js/runtime-main.b79ec780.js"
+  },
+  {
+    "revision": "f2712925e491d33a2cfe2ecdb127f0ef",
+    "url": "/abutto-web/static/media/Group 10.f2712925.png"
+  },
+  {
+    "revision": "c6a7628fb06c710d50ab6fa6ff82d57b",
+    "url": "/abutto-web/static/media/blue.c6a7628f.png"
+  },
+  {
+    "revision": "28835c3205ff9df5a31f6edbd1c92f1b",
+    "url": "/abutto-web/static/media/bosky.28835c32.png"
+  },
+  {
+    "revision": "957d6ba28ba85ee21a47f924794b98c0",
+    "url": "/abutto-web/static/media/curve-rect.957d6ba2.svg"
+  },
+  {
+    "revision": "dfbb5bdd287d9ae685b2d488a53f5737",
+    "url": "/abutto-web/static/media/fb_logo.dfbb5bdd.svg"
+  },
+  {
+    "revision": "211bdcc4cde36834a06c12108f327928",
+    "url": "/abutto-web/static/media/footer-logo.211bdcc4.svg"
+  },
+  {
+    "revision": "4bdf157ce31dd2afdf8edad47809f94a",
+    "url": "/abutto-web/static/media/googleplay.4bdf157c.svg"
+  },
+  {
+    "revision": "776d17a331a1b1eda91409a29ca97ab6",
+    "url": "/abutto-web/static/media/iphonePic.776d17a3.svg"
+  },
+  {
+    "revision": "7986b7ef0b7c4719705755b2fcc6a601",
+    "url": "/abutto-web/static/media/landpage-card.7986b7ef.svg"
+  },
+  {
+    "revision": "609b17c973536856e32916b3e77bab7a",
+    "url": "/abutto-web/static/media/link_bg.609b17c9.svg"
+  },
+  {
+    "revision": "321382e5f0849d2f6cacfdd863ef196e",
+    "url": "/abutto-web/static/media/logo_image.321382e5.svg"
+  },
+  {
+    "revision": "dac6d3637cfbab229c276ff1b4f07215",
+    "url": "/abutto-web/static/media/money.dac6d363.svg"
+  },
+  {
+    "revision": "642005ddb44cce6d14bedf4bf18b9cc9",
+    "url": "/abutto-web/static/media/super-g.642005dd.svg"
+  },
+  {
+    "revision": "d21bfe9d8daabf32e834facdd524c21d",
+    "url": "/abutto-web/static/media/support.d21bfe9d.svg"
+  },
+  {
+    "revision": "b30aa6054474c55efdadc2d01bd4a858",
+    "url": "/abutto-web/static/media/testimoy_female.b30aa605.svg"
+  },
+  {
+    "revision": "10d6035ead5c3cc8a4c9b58887a156fc",
+    "url": "/abutto-web/static/media/testimoy_male.10d6035e.svg"
+  }
+]);
